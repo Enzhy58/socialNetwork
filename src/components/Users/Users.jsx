84e9ff756989c7e41a59f5f3b7f883b8f45345d3
Enzhy58 <ang.borisova2015@yandex.ru>
2024-02-14 17:55:04 +0300

    axios has been connected

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import './Users.css';
+import axios from 'axios';
 
 const Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-            { id: 1, photoUrl: 'https://i.pinimg.com/originals/59/f6/d1/59f6d192b9db45b87c09ebdfb184b44d.png', followed: true, fullName: 'Dmitry', status: 'I am looking for a Job right now...', location: { city: 'Minsk', country: 'Belarus' } },
-            { id: 2, photoUrl: 'https://sun9-21.userapi.com/impf/c631229/v631229299/3bc11/A8aMnPzdtiE.jpg?size=800x776&quality=96&sign=d82fbe65ec309e98fc4aa15587dde201&c_uniq_tag=Fo65Ekl0t60bxUiJTxIlJFOuy58vMnIOSh_R7eE51No&type=album', followed: true, fullName: 'Svetlana', status: 'I am so pretty', location: { city: 'Moscow', country: 'Russia' } },
-            { id: 3, photoUrl: 'https://www.ansaroo.com/images/5e/5e78dcf7f83f8994ba19e1448add1afb.jpeg', followed: false, fullName: 'Sergei', status: 'I like a football!!!', location: { city: 'Kiev', country: 'Ukrane' } },
-            { id: 4, photoUrl: 'https://66.media.tumblr.com/8676feb22c8037728ca627d06f83a091/tumblr_orilg1U0E31s3zxg3o1_500.jpg', followed: false, fullName: 'Andrew', status: 'I am free to help you to create good Video Production', location: { city: 'USA', country: 'Philadelphia' } }
-        ]
 
-        )
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items)
+        });
+
     }
 
     return <div>
@@ -22,7 +20,7 @@ const Users = (props) => {
 
                     <div>
 
-                        <img className="users__img" src={u.photoUrl} alt="" />
+                        <img className="users__img" src={u.photos.small} alt="" />
 
                         <div>
                             {u.followed
@@ -34,12 +32,12 @@ const Users = (props) => {
 
                     <div className="users__wrapper">
                         <div className="users__info">
-                            <div className="users__name">{u.fullName}</div>
+                            <div className="users__name">{u.name}</div>
                             <div className="users__status">{u.status}</div>
                         </div>
                         <div className="users__location">
-                            <div>{u.location.country},</div>
-                            <div>{u.location.city}</div>
+                            <div>{"u.location.country"},</div>
+                            <div>{"u.location.city"}</div>
                         </div>
                     </div>
                 </li>)
@@ -50,3 +48,8 @@ const Users = (props) => {
 }
 
 export default Users;
+
+// { id: 1, photoUrl: 'https://i.pinimg.com/originals/59/f6/d1/59f6d192b9db45b87c09ebdfb184b44d.png', followed: true, fullName: 'Dmitry', status: 'I am looking for a Job right now...', location: { city: 'Minsk', country: 'Belarus' } },
+// { id: 2, photoUrl: 'https://sun9-21.userapi.com/impf/c631229/v631229299/3bc11/A8aMnPzdtiE.jpg?size=800x776&quality=96&sign=d82fbe65ec309e98fc4aa15587dde201&c_uniq_tag=Fo65Ekl0t60bxUiJTxIlJFOuy58vMnIOSh_R7eE51No&type=album', followed: true, fullName: 'Svetlana', status: 'I am so pretty', location: { city: 'Moscow', country: 'Russia' } },
+// { id: 3, photoUrl: 'https://www.ansaroo.com/images/5e/5e78dcf7f83f8994ba19e1448add1afb.jpeg', followed: false, fullName: 'Sergei', status: 'I like a football!!!', location: { city: 'Kiev', country: 'Ukrane' } },
+// { id: 4, photoUrl: 'https://66.media.tumblr.com/8676feb22c8037728ca627d06f83a091/tumblr_orilg1U0E31s3zxg3o1_500.jpg', followed: false, fullName: 'Andrew', status: 'I am free to help you to create good Video Production', location: { city: 'USA', country: 'Philadelphia' } }
