57171b0c9eff10bfa27df6c7ca79eda235fa5c50
Enzhy58 <ang.borisova2015@yandex.ru>
2024-02-17 11:24:56 +0300

    refactoring users component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,74 +1,53 @@
-import React from 'react';
 import styles from './Users.module.css';
-import axios from 'axios';
 import userPhoto from '../../assets/images/userPhoto.jpg';
 
-class Users extends React.Component {
+let Users = (props) => {
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items);
-            this.props.setTotalUsersCount(response.data.totalCount);
-        });
-    };
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-        });
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
+    return <div>
+        <ul className={styles.pagination}>
+            {pages.map(p => {
+                return <li className={props.currentPage === p && styles.selected} onClick={(e) => { props.onPageChanged(p) }}>{p}</li>
+            })}
+        </ul>
 
-    render() {
+        <ul className={styles.list}>
+            {
+                props.users.map(u => <li className={styles.item} key={u.id} >
 
-        let pagesCount = Math.ceil (this.props.totalUsersCount / this.props.pageSize);
+                    <div>
 
-        let pages = [];
-        for(let i = 1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-
-        return <div>
-            <ul className={styles.pagination}>
-                {pages.map(p => {
-                    return <li className={this.props.currentPage === p && styles.selected } onClick={ (e) => {this.onPageChanged(p)} }>{p}</li>
-                })}
-            </ul>
-
-            <ul className={styles.list}>
-                {
-                    this.props.users.map(u => <li className={styles.item} key={u.id} >
+                        <img className={styles.img} src={u.photos.small != null ? u.photos.small : userPhoto} alt="" />
 
                         <div>
+                            {u.followed
+                                ? <button className={styles.btn} onClick={() => { props.unfollow(u.id) }} >Unfollow</button>
+                                : <button className={styles.btn} onClick={() => { props.follow(u.id) }}>Follow</button>}
 
-                            <img className={styles.img} src={u.photos.small != null ? u.photos.small : userPhoto} alt="" />
-
-                            <div>
-                                {u.followed
-                                    ? <button className={styles.btn} onClick={() => { this.props.unfollow(u.id) }} >Unfollow</button>
-                                    : <button className={styles.btn} onClick={() => { this.props.follow(u.id) }}>Follow</button>}
-
-                            </div>
                         </div>
+                    </div>
 
-                        <div className={styles.wrapper}>
-                            <div className={styles.info}>
-                                <div className={styles.name}>{u.name}</div>
-                                <div className={styles.status}>{u.status}</div>
-                            </div>
-                            <div className={styles.location}>
-                                <div>{"u.location.country"},</div>
-                                <div>{"u.location.city"}</div>
-                            </div>
+                    <div className={styles.wrapper}>
+                        <div className={styles.info}>
+                            <div className={styles.name}>{u.name}</div>
+                            <div className={styles.status}>{u.status}</div>
                         </div>
-                    </li>)
-                }
-            </ul>
-            <button className={styles.btn}>Show more</button>
-        </div>
-    }
-
+                        <div className={styles.location}>
+                            <div>{"u.location.country"},</div>
+                            <div>{"u.location.city"}</div>
+                        </div>
+                    </div>
+                </li>)
+            }
+        </ul>
+        <button className={styles.btn}>Show more</button>
+    </div>
 }
 
 export default Users;
